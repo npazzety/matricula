c62616c1581a9a83586ff5b5a709cd8dbf99a833
npazzety <nelsonpazzety10@gmail.com>
2024-04-11 18:06:13 -0600

    proyecto

---
+++ src/main/java/hn/unah/matricula/MatriculaApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.matricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaApplication.class, args);
+	}
+
+}
