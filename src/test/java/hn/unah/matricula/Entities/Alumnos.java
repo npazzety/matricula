2912b727d0fe5af7c1fc8ee2b5ee0d7e21398570
npazzety <nelsonpazzety10@gmail.com>
2024-04-13 17:51:01 -0600

    proyecto

---
+++ src/test/java/hn/unah/matricula/Entities/Alumnos.java
@@ -0,0 +1,42 @@
+package hn.unah.matricula.Entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "alumnos")
+
+public class Alumnos {
+
+
+    @Id
+    @Column(name = "numerocuenta")
+    private String numeroCuenta;
+
+    private String nombre;
+
+    private String apellido;
+
+    private boolean sexo;
+
+    private String direccion;
+
+    private String correo;
+
+    private String foto;
+
+    private int indice;
+
+    private String contraseña;
+
+
+    @Column(name = "fechacreacion")
+    private Date fechaCreacion;
+}
+
