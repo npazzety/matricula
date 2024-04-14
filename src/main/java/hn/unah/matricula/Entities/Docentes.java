077ed499a9458002917a24fc37435ea0eb5c0dff
npazzety <nelsonpazzety10@gmail.com>
2024-04-13 18:48:08 -0600

    ya

---
+++ src/main/java/hn/unah/matricula/Entities/Docentes.java
@@ -0,0 +1,12 @@
+package hn.unah.matricula.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "docentes")
+@Entity
+public class Docentes {
+    
+}
