c667c04957b30ea712bea3698ee1b3b482a0dc20
npazzety <nelsonpazzety10@gmail.com>
2024-04-13 20:02:46 -0600

    nuevo

---
+++ src/main/java/hn/unah/matricula/Entities/Coordinador.java
@@ -0,0 +1,49 @@
+package hn.unah.matricula.Entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "coordinador")
+public class Coordinador {
+
+    @Id
+    private String dni;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String direccion;
+
+
+    @JoinColumn(name = "idcarrera", referencedColumnName = "idcarrera")
+    @OneToOne
+    private Carreras carreras;
+
+    private String correo;
+
+    private String telefono;
+
+    private String foto;
+
+    private String especializacion;
+
+    private String contraseña;
+
+
+    @Column(name = "fechacoodinador")
+    private Date fechaCoordinador ;
+
+    private boolean sexo;
+    
+}
--- src/main/java/hn/unah/matricula/Entities/Docentes.java
+++ src/main/java/hn/unah/matricula/Entities/Docentes.java
@@ -1,12 +1,42 @@
 package hn.unah.matricula.Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Table(name = "docentes")
 @Entity
+
 public class Docentes {
-    
+
+    @Id
+    @Column(name = "numerocuenta")
+    private String numeroCuenta;
+
+    private String nombre;
+
+    private String apellido;
+
+    private boolean sexo;
+
+    private String direccion;
+
+    private String corre;
+
+    private String foto;
+
+    private String especializacion;
+
+    private int likes;
+
+    private int dislikes;
+
+    private String contraseña;
+
+    @Column(name = "fechacontratacion")
+    private int fechaContratacion;
+
 }
