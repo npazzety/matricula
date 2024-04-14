1b1416cc338f0b960fab3f6d40f5c49bbb112f01
npazzety <nelsonpazzety10@gmail.com>
2024-04-13 21:36:04 -0600

    MM

--- src/main/java/hn/unah/matricula/Entities/ReporteAlumno.java
+++ src/main/java/hn/unah/matricula/Entities/ReporteAlumno.java
@@ -5,32 +5,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "historialalumno")
-public class HistorialAlumno {
+@Table(name = "reportealumno")
 
+public class ReporteAlumno {
 
     @Id
-    @Column(name = "idhistorial")
-    private int idHistorial;
+    @Column(name = "idreporte")
+    private int reporte;
 
-    @OneToOne()
-    @JoinColumn(name = "numerocuenta", referencedColumnName = "numerocuenta")
+    @JoinColumn(name = "idalumno", referencedColumnName = "idalumno")
+    @OneToMany
     private Alumnos alumnos;
 
-    @Id
     @JoinColumn(name = "idclase", referencedColumnName = "idclase")
-    @OneToMany()
+    @OneToMany
     private Clases clases;
 
-    @JoinColumn(name = "idreporte", referencedColumnName = "idreporte")
-    @OneToOne
-    private ReporteAlumno reportealumno;
+    @JoinColumn(name = "idperiodo", referencedColumnName = "idperiodo")
+    @OneToMany
+    private Periodos periodos;
+
+    private int nota;
+
+    private String observacion;
 
     
 }
