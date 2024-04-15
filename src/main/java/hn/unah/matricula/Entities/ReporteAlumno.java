83ae5344a3d8aaed23c27050896c069f87ec021c
npazzety <nelsonpazzety10@gmail.com>
2024-04-15 01:59:12 -0600

    onetoone

--- src/main/java/hn/unah/matricula/Entities/ReporteAlumno.java
+++ src/main/java/hn/unah/matricula/Entities/ReporteAlumno.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,7 +21,7 @@ public class ReporteAlumno {
     private int reporte;
 
     @JoinColumn(name = "idalumno", referencedColumnName = "idalumno")
-    @OneToMany
+    @OneToOne
     private Alumnos alumnos;
 
     @JoinColumn(name = "idclase", referencedColumnName = "idclase")
