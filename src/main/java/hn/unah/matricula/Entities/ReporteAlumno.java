e8c231a1ba7b730244f1702091565435e00efaf3
npazzety <nelsonpazzety10@gmail.com>
2024-04-16 17:23:21 -0600

    MM

--- src/main/java/hn/unah/matricula/Entities/ReporteAlumno.java
+++ src/main/java/hn/unah/matricula/Entities/ReporteAlumno.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
