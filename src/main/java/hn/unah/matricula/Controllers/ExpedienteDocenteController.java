e9ff9b7f5e6ef314d9683bc26241de094ab59c9c
npazzety <nelsonpazzety10@gmail.com>
2024-04-19 00:56:42 -0600

    JJ

--- src/main/java/hn/unah/matricula/Controllers/ExpedienteDocenteController.java
+++ src/main/java/hn/unah/matricula/Controllers/ExpedienteDocenteController.java
@@ -3,6 +3,7 @@ package hn.unah.matricula.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hn.unah.matricula.Entities.ExpedienteDocente;
 import hn.unah.matricula.Services.impl.ExpedienteDocenteServiceImpl;
 
 import java.util.List;
@@ -10,12 +11,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
-<<<<<<< HEAD:src/main/java/hn/unah/matricula/Controllers/ExpedienteDocente.java
-=======
-import hn.unah.matricula.Entities.ExpedienteDocente;
-
-
->>>>>>> 628fa494464d18362bf7735c44cc4dd37030f585:src/main/java/hn/unah/matricula/Controllers/ExpedienteDocenteController.java
 @RestController
 @RequestMapping("api/matricula/expedienteDocente")
 
