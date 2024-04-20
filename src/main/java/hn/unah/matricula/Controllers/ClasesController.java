a46b39b47e0f53943a245a28d998e6913e279f21
aljosama23 <alejomartiz10@gmail.com>
2024-04-20 16:12:52 -0600

    si

--- src/main/java/hn/unah/matricula/Controllers/ClasesController.java
+++ src/main/java/hn/unah/matricula/Controllers/ClasesController.java
@@ -1,10 +1,27 @@
 package hn.unah.matricula.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import hn.unah.matricula.Entities.Clases;
+import hn.unah.matricula.Services.impl.ClasesServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/matricula/clases")
 public class ClasesController {
     
+    @Autowired
+    private ClasesServiceImpl clasesServiceImpl;
+
+    @Operation(summary = "Obtiene las clases por medio del numero de cuenta del docente")
+    @GetMapping("/numeroCuentaDocente")
+    public List<Clases> obtenerClasesDeDocente(@RequestParam String idDocente) {
+        return clasesServiceImpl.obtenerClasesDeDocente(idDocente);
+    }
 }
