2357e1a1cf52d7e235d6ffed5521a5285dccea1d
Dragonslayer <marlonjeancarlo@gmail.com>
2024-04-21 20:20:18 -0600

    Se implementa obtenencion de Secciones por Docente

--- src/main/java/hn/unah/matricula/Entities/Docentes.java
+++ src/main/java/hn/unah/matricula/Entities/Docentes.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -67,4 +68,6 @@ public class Docentes {
     @JsonIgnore
     private HistorialDocente historialDocente;
 
+    @OneToMany(mappedBy = "docentes")
+    private List<Seccion> secciones;
 }
